11e6d841b2db04ab8bfcd1fc2be9f7b7ca779358
mert2kk <mertkadirkayan@gmail.com>
2025-11-15 02:47:57 +0300

    design and colors changed in recipe card

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-      <div className="flex justify-between items-center p-4 bg-[#173B61] flex-row w-screen">
+      <div className="flex justify-between items-center p-4 bg-[#242d4c] flex-row w-screen">
           <h1>MyRecipeDeck</h1>
           <button >Sign Out</button>
       </div>
