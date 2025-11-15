5de06a9c1881d553c7c7827fbb3efa4322e8215d
mert2kk <mertkadirkayan@gmail.com>
2025-11-15 04:05:16 +0300

    Menu is added

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -10,6 +10,16 @@ export default function Header() {
         alt="logo"
         className="ml-10 p-2"
       />
+      <div>
+        <button className="p-4 font-semibold text-sm ">My Recipes</button>
+        <button className="p-4 font-semibold text-sm ">Collections</button>
+        <button className="p-4 font-semibold text-sm ">Add Recipe</button>
+
+        <button className="p-4 font-semibold text-sm ">
+          Track Macros & Calories
+        </button>
+        <button className="p-4 font-semibold text-sm ">Settings</button>
+      </div>
       <button className="p-4 font-semibold text-sm ">Sign Out</button>
     </div>
   )
