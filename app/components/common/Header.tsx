b82b3104d29e9755e2dc7f89e50f869cb8f39f5e
mert2kk <mertkadirkayan@gmail.com>
2025-11-15 03:45:56 +0300

    Logo is added

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+
 export default function Header() {
   return (
-    <div className="flex justify-between items-center p-4 bg-[#242d4c] flex-row w-screen">
-      <h1>MyRecipeDeck</h1>
-      <button>Sign Out</button>
+    <div className="flex justify-between items-center p-4 bg-[#242d4c] flex-row w-screen h-28">
+      <Image
+        src="/images/myRecipeDeck.png"
+        width={100}
+        height={100}
+        alt="logo"
+        className="ml-10 p-2"
+      />
+      <button className="p-4 font-semibold text-sm ">Sign Out</button>
     </div>
   )
 }
