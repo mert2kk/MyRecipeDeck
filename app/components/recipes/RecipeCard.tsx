4037d1caa2cebcc327e12726b5cf60d2f40e4c97
mert2kk <mertkadirkayan@gmail.com>
2025-11-16 00:26:15 +0300

    css fixes

--- app/components/recipes/RecipeCard.tsx
+++ app/components/recipes/RecipeCard.tsx
@@ -9,7 +9,16 @@ import FolderIcon from '../Icons/FolderIcon'
 export default function recipeCard() {
   const router = useRouter()
   return (
-    <div className="w-[500px] bg-[#f4f0e5] h-[250px] m-4 flex flex-row rounded-lg   ">
+    <div
+      className="w-[500px] bg-[#f4f0e5] h-[250px] m-4 flex flex-row rounded-lg 
+    shadow-[0_10px_25px_rgba(0,0,0,0.15),0_20px_50px_rgba(0,0,0,0.1)]
+         transition-all duration-300 ease-in-out
+         hover:shadow-[0_15px_40px_rgba(0,0,0,0.2),0_25px_60px_rgba(0,0,0,0.12)]
+         hover:-translate-y-1
+         hover:scale-101
+         hover:backdrop-brightness-102
+    "
+    >
       <div className="w-2/5 flex items-center m-4 ">
         <Image
           src="/images/Pasta.jpg"
@@ -22,11 +31,11 @@ export default function recipeCard() {
       </div>
       <div className="w-3/5 flex flex-col p-2 justify-center m-4">
         <div>
-          <button className="flex float-right">
+          <button className="flex float-right cursor-pointer">
             <FilledHeartIcon />
             <EmptyHeartIcon />
           </button>
-          <h1 className="text-gray-500 uppercase text-xs flex flex-row gap-2 items-center">
+          <h1 className="text-gray-500 uppercase text-xs flex flex-row gap-2 items-center cursor-pointer">
             <span>
               <FolderIcon />
             </span>
@@ -56,12 +65,12 @@ export default function recipeCard() {
         <p className="text-xs font-semibold text-gray-600">Main Dish</p>
         <div className="flex flex-row mt-4  ">
           <button
-            className="bg-[#fc4126] text-[#FFEBD0] px-4 py-2 rounded-2xl text-[12px] font-semibold "
+            className="bg-[#fc4126] text-[#FFEBD0] px-4 py-2 rounded-2xl text-[12px] font-semibold cursor-pointer "
             onClick={() => router.push('/recipes/123')}
           >
             View Recipe
           </button>
-          <button className="bg-[#f4f0e5] text-gray-500 font-semibold text-[12px] ml-4 flex flex-row items-center justify-center space-x-2 ">
+          <button className="bg-[#f4f0e5] text-gray-500 font-semibold text-[12px] ml-4 flex flex-row items-center justify-center space-x-2 cursor-pointer ">
             <AddFolderIcon />
             <p>Add to Collection</p>
           </button>
