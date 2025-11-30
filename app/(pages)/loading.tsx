5d1b382f63a23384c3e2bcf76ed18055c3335011
mert2kk <mertkadirkayan@gmail.com>
2025-11-30 05:02:54 +0300

    loading is updated

--- app/(pages)/loading.tsx
+++ app/(pages)/loading.tsx
@@ -1,10 +1,15 @@
 export default function Loading() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-white">
-      {/* Buraya şık bir GIF veya CSS Loader koyabilirsin */}
-      <div className="flex flex-col items-center gap-4">
-        <div className="h-12 w-12 animate-spin rounded-full border-4 border-brand-dark border-t-transparent"></div>
-        <p className="text-gray-500 font-medium">Tarifler Hazırlanıyor...</p>
+    <div className="w-full h-[calc(100vh-112px)] flex flex-col items-center justify-center bg-brand-bg">
+      <div className="relative flex items-center justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          src="/PreparingFood.webm"
+          className="relative w-64 h-64 object-contain drop-shadow-lg z-10"
+        />
       </div>
     </div>
   )
